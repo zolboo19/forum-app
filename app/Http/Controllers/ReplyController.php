597f80b3dcb91f778b85aa82511b59deef5a9d6a
zolboo19 <zolboo0906@gmail.com>
2021-01-10 13:09:36 +0800

    Reply store and delete method

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Question;
 use App\Models\Reply;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ReplyController extends Controller
 {
@@ -17,5 +18,16 @@ class ReplyController extends Controller
     public function show(Question $question, Reply $reply)
     {
         return $reply;
+    }
+    public function store(Question $question, Request $request)
+    {
+        $question->replies()->create($request->all());
+        return response('Сэтгэгдлийг амжилттай үүсгэлээ.', Response::HTTP_CREATED);
+    }
+
+    public function destroy(Question $question, Reply $reply)
+    {
+        $reply->delete();
+        return response(null, Response::HTTP_NOT_FOUND);
     }
 }
