23a6297eb2dedd55e3c8095bb694c9258e963e87
zolboo19 <zolboo0906@gmail.com>
2021-01-10 11:33:32 +0800

    Category read show store delete methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return Category::latest()->get();
+    }
+    public function store(Request $request)
+    {
+        // Category::create($request->all());
+        $category = new Category;
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->save();
+        return response('Категорыг амжилттай үүсгэлээ.', Response::HTTP_CREATED);
+    }
+    public function show(Category $category)
+    {
+        return $category;
+    }
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::apiResource('/questions', QuestionController::class);
+Route::apiResource('/categories', CategoryController::class);
