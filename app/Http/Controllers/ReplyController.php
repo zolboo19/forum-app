6bb35d650b303eeda409331a7295d69a97540c0d
zolboo19 <zolboo0906@gmail.com>
2021-01-10 12:48:39 +0800

    Reply read and show methods

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+use App\Models\Reply;
 use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
-    //
+    public function index(Question $question)
+    {
+        return $question->replies;
+        // return Reply::latest()->get();
+    }
+
+    public function show(Question $question, Reply $reply)
+    {
+        return $reply;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\ReplyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/questions', QuestionController::class);
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/questions/{question}/replies', ReplyController::class);
